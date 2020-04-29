24fe6767dfafbb7015809abfd62aec538e9c0b60
mitchmindtree <mitchell.nordine@gmail.com>
2020-04-29 17:55:05 +0200

    Add extensive top-level API docs, mirroring README

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,88 @@
 //! A complete implementation of the ILDA Image Data Transfer Format Specification, Revision 011,
 //! 2014-11-16.
+//!
+//! This library provides efficient reading and writing of IDTF. The reader implementation uses a
+//! zero-copy approach where structures are mapped directly to the memory from which they are read.
+//!
+//! ## Usage
+//!
+//! The [**SectionReader**][1] type can be used to read IDTF sections from any type implementing
+//! `std::io::Read`. This allows for efficiently reading the format from any byte source (e.g.
+//! file, memory, socket, etc).
+//!
+//! ```rust,no_run
+//! # let mut reader = &[0u8][..];
+//! let mut reader = ilda_idtf::SectionReader::new(reader);
+//! ```
+//!
+//! The [**open**][2] function is provided as a convenience for opening a buffered IDTF
+//! **SectionReader** for the file at the given path.
+//!
+//! ```rust,no_run
+//! # let path = "/foo/bar";
+//! let mut reader = ilda_idtf::open(path).unwrap();
+//! ```
+//!
+//! The [**SectionReader::read_next**][3] method allows for iteration over the sections contained
+//! within.
+//!
+//! ```rust,no_run
+//! # let mut reader = ilda_idtf::open("/foo/bar").unwrap();
+//! while let Ok(Some(section)) = reader.read_next() {
+//!     // ...
+//! }
+//! ```
+//!
+//! Each yielded [**Section**][4] provides access to the [**Header**][5] and the inner `reader`.
+//! The exact `reader` kind is determined via the [**Format**][6] specified within the header. The
+//! user must pattern match on the section's `reader` field in order to retrieve an instance of the
+//! correct subsection reader type. The user may then read the associated subsection data.
+//!
+//! ```rust,no_run
+//! # let mut reader = ilda_idtf::open("/foo/bar").unwrap();
+//! # while let Ok(Some(section)) = reader.read_next() {
+//! match section.reader {
+//!     ilda_idtf::SubsectionReaderKind::Coords3dIndexedColor(mut r) => {
+//!         while let Some(point) = r.read_next().unwrap() {
+//!             // ...
+//!         }
+//!     }
+//!     ilda_idtf::SubsectionReaderKind::Coords2dIndexedColor(mut r) => {
+//!         while let Some(point) = r.read_next().unwrap() {
+//!             // ...
+//!         }
+//!     }
+//!     ilda_idtf::SubsectionReaderKind::ColorPalette(mut r) => {
+//!         while let Some(palette) = r.read_next().unwrap() {
+//!             // ...
+//!         }
+//!     }
+//!     ilda_idtf::SubsectionReaderKind::Coords3dTrueColor(mut r) => {
+//!         while let Some(point) = r.read_next().unwrap() {
+//!             // ...
+//!         }
+//!     }
+//!     ilda_idtf::SubsectionReaderKind::Coords2dTrueColor(mut r) => {
+//!         while let Some(point) = r.read_next().unwrap() {
+//!             // ...
+//!         }
+//!     }
+//! }
+//! # }
+//! ```
+//!
+//! In order to interpret the indexed color data formats, a color palette must be used. A color
+//! palette is an ordered list of RGB colors. While the palette *should* be specified by a
+//! preceding `Section`, this is not always the case. The ILDA IDTF specification recommends a
+//! default palette. This palette is provided via the [**DEFAULT_PALETTE**][7] constant.
+//!
+//! [1]: struct.SectionReader.html
+//! [2]: fn.open.html
+//! [3]: struct.SectionReader.html#method.read_next
+//! [4]: struct.Section.html
+//! [5]: layout/struct.Header.html
+//! [6]: layout/struct.Format.html
+//! [7]: constant.DEFAULT_PALETTE.html
 
 #[macro_use]
 extern crate bitflags;
